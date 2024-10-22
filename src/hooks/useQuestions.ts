a23d4cfe5afdbd6a31cadcd8b4e61df58cade5b4
Tagir Sharipov <tsharipov@keybiz.it>
2024-10-22 18:23:03 +0200

    added control for re-fetching

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -1,19 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fetchQuestions } from "../ api/api";
 import { Question } from "../types";
 
 export default function useQuestions() {
   const [questions, setQuestions] = useState<Question[]>([]);
+  const fetched = useRef(false); //to prevent occasional data re-fetching. Cab be removed if not needed
 
   useEffect(() => {
+    if (fetched.current) return;
     fetchQuestions()
       .then(res => res.json())
       .then(response => {
         setQuestions(response);
+        fetched.current = true;
       })
       .catch(err => {
         throw err;
       });
-  }, []);
+  }, [fetched]);
   return questions;
 }
