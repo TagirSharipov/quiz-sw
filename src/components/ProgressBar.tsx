8a7cbfa225b120fca5903088897fa03e7c7e1749
Tagir Sharipov <tsharipov@keybiz.it>
2024-10-22 17:26:58 +0200

    changed file extension

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,12 +1,19 @@
 import { ProgressBar as PrimeProgressBar } from "primereact/progressbar";
+import { Question, UserAnswer } from "../types";
 
-export default function ProgressBar({ answers, questions }) {
+export default function ProgressBar({
+  answers,
+  questions,
+}: {
+  answers: UserAnswer[];
+  questions: Question[];
+}) {
   const value = answers.filter(a => a.confirmed).length;
 
   return (
     <PrimeProgressBar
       value={Math.ceil((value / questions.length) * 100)}
-      displayValueTemplate={value => ``}
+      displayValueTemplate={() => ""}
     />
   );
 }
